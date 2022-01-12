9b2f33bdee4bd5937f5699c76596ce0631712628
mayurlalwani <mayur_lalwani4@ymail.com>
2022-01-12 18:04:18 +0530

    Implemented signup page in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
 import Navbar from "./components/Navbar";
 
 import "./App.scss";
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <div className="App">
         <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
       </div>
     </Router>
   );
---
+++ frontend/src/components/RegisterPage.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../actions/userActions";
+import { register } from "../actions/userActions";
+
+import "./register.scss";
+
+const RegisterPage = ({ location, history }) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, error, userInfo } = userRegister;
+
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage("Passwords do not match!!");
+    } else {
+      dispatch(register(firstName, lastName, email, password));
+    }
+  };
+
+  return (
+    <div className="signup-container">
+      <h1>Sign Up</h1>
+      <form onSubmit={submitHandler} className="signup-form">
+        <input
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button type="submit" className="login-btn">
+          Sign Up
+        </button>
+      </form>
+      Have an Account?
+      <Link to={redirect ? `/login?redirect=${redirect}` : "/login"}>
+        Login
+      </Link>
+    </div>
+  );
+};
+
+export default RegisterPage;
